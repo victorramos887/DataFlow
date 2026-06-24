55a1a1c93d0d83a805889695a75217ea3991dc0c
Victor Ramos <v.ramos58@hotmail.com>
2026-06-24 08:16:59 -0300

    feat: repos roles create

--- api_fastapi/app/features/auth/repository/roles_repository.py
+++ api_fastapi/app/features/auth/repository/roles_repository.py
@@ -1,12 +1,25 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.features.auth.entities.roles_entity import Roles
 
-
+from app.features.auth.models.role_model import RoleModel
 
 class RolesRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
         
     async def create(self, roles: Roles) -> Roles:
-        pass
+        role_model = RoleModel(
+            name = roles.name,
+            description = roles.description
+        )
+        
+        self.session.add(role_model)
+        await self.session.commit()
+        await self.session.refresh(role_model)
+        
+        return Roles(
+            id=role_model.id,
+            name=role_model.name,
+            description=role_model.description
+        )
         
